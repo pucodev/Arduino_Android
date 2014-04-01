efd58c70b454ae5362d1fc237d69f4e078ef5364
Jorge Estrada <jor1550g@gmail.com>
2014-04-01 01:31:03 -0500

    step-8

--- arduino_app/ledArduino/ledArduino.ino
+++ arduino_app/ledArduino/ledArduino.ino
@@ -12,13 +12,30 @@ int led = 13;
 // the setup routine runs once when you press reset:
 void setup() {                
   // initialize the digital pin as an output.
-  pinMode(led, OUTPUT);     
+  pinMode(led, OUTPUT);
+  
+  // Configuramos Puerto Serial
+  Serial.begin(9600);
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
-  digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);               // wait for a second
-  digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);               // wait for a second
+  byte value;
+  
+  // leemos valor serial
+  value =  Serial.read();
+  
+  switch(value){
+    case 48:
+      digitalWrite(led, HIGH);
+      break;
+    case 49:
+      digitalWrite(led, LOW);
+      break;
+  }
+  
+  //  digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
+//  delay(1000);               // wait for a second
+//  digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
+//  delay(1000);               // wait for a second
 }
